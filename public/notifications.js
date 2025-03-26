ec12b09df69f6b72299c7c75609187f3ef37fd0d
marcd <marc.davies@adyen.com>
2025-03-26 22:41:56 +0100

    change to websockets

--- public/notifications.js
+++ public/notifications.js
@@ -17,18 +17,25 @@ eventSource.onerror = function () {
 
 //handle the expansion of elements on click
 
-const allMessages = document.querySelectorAll("#messages");
-if(allMessages) {
+// const allMessages = document.querySelectorAll(".message");
+// if(allMessages) {
 
-allMessages.forEach(function (message) {
-    message.onclick = function () {
-        // Toggle between maxHeight 50px and 200px
-        if (this.style.maxHeight === "50px") {
-            this.style.maxHeight = "200px";
-        } else {
-            this.style.maxHeight = "50px";
-        }
-    };
-})
-}
+// allMessages.forEach(function (message) {
+//     message.onclick = function () {
+//         // Toggle between maxHeight 50px and 200px
+//         if (this.style.maxHeight === "50px") {
+//             this.style.maxHeight = "200px";
+//         } else {
+//             this.style.maxHeight = "50px";
+//         }
+//     };
+// })
+// }
+
+document.getElementById("messages").addEventListener("click", function (event) {
+    if (event.target.classList.contains("message")) {
+        // Toggle height between 50px and 200px
+        event.target.style.maxHeight = event.target.style.maxHeight === "50px" ? "200px" : "50px";
+    }
+});
 
