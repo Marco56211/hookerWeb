b3eb6ccec1bb3f19239bcf13883120c0b99252b5
marcd <marc.davies@adyen.com>
2025-03-26 21:45:51 +0100

    change to websockets

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.post("/webhook", (req, res) => {
 });
 
 // Serve the index.html (or other entry point)
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 // Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, "public"), {
